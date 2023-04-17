78f62132b571f23178be2b4c81a84869d3b96456
Ilya <quicksilya@gmail.com>
2023-04-17 21:30:51 +0500

    Добавил ChatManager (http запросы) и Взаимодействие с серверной частью при помощи WebAPI

--- BlazorChat/Client/Program.cs
+++ BlazorChat/Client/Program.cs
@@ -19,6 +19,17 @@ namespace BlazorChat.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            //регистрирует HttpClient, который будет использоваться для отправки запросов к серверу
+            builder.Services.AddHttpClient("BlazorChat.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+            //регистрируется клиент
+            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorChat.ServerAPI"));
+            //добавляются настройки для библиотеки MudBlazor
+            builder.Services.AddMudServices(c => { c.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight; });
+            //cервис, который будет использоваться для авторизации пользователей на сервере
+            builder.Services.AddApiAuthorization();
+            //регистрируется класс ChatManager, который будет использоваться для управления чатом
+            builder.Services.AddTransient<IChatManager, ChatManager>();
             await builder.Build().RunAsync();
         }
     }
