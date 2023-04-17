6440fdcc1acb31fe23b8709857965b97e1858b7b
Maven <quicksilya@gmail.com>
2023-04-17 21:00:21 +0500

    Добавил ChatManager( http запросы к клиенту для получения данных о чате/пользователе/сообщениях) и SignalRHub

--- BlazorChat/Server/Startup.cs
+++ BlazorChat/Server/Startup.cs
@@ -19,8 +19,6 @@ namespace BlazorChat.Server
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSignalR();
@@ -34,7 +32,6 @@ namespace BlazorChat.Server
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -46,7 +43,6 @@ namespace BlazorChat.Server
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
