909f391715e5d9ad3b9af1f0fbf978498b7f1d9d
Kirill Skorikov <92673983+skorikovkr@users.noreply.github.com>
2023-04-19 21:43:54 +0500

    добавил метод уведомления о получении сообщения в концетраторе

--- BlazorChat/Server/SignalRHub.cs
+++ BlazorChat/Server/SignalRHub.cs
@@ -13,5 +13,10 @@ namespace BlazorChat.Server
         {
             await Clients.All.SendAsync("ReceiveMessage", message, userName);
         }
+
+        public async Task ChatNotificationAsync(string message, string receiverUserId, string senderUserId)
+        {
+            await Clients.All.SendAsync("ReceiveChatNotification", message, receiverUserId, senderUserId);
+        }
     }
 }
