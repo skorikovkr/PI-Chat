ba312b820566d1f5fe3325ba2d2952429d567bd0
Ilya <quicksilya@gmail.com>
2023-04-22 03:52:08 +0500

    Добавил примервый вид страницы чата и отправление сообщения

---
+++ BlazorChat/Client/Pages/Chat.razor.cs
@@ -0,0 +1,45 @@
+﻿using BlazorChat.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.JSInterop;
+using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorChat.Client.Pages
+{
+    public partial class Chat
+    {
+       
+        [CascadingParameter] public HubConnection hubConnection { get; set; }
+        [Parameter] public string CurrentMessage { get; set; }
+        [Parameter] public string CurrentUserId { get; set; }
+        [Parameter] public string CurrentUserName { get; set; }
+
+       
+        public List<ApplicationUser> ChatUsers = new List<ApplicationUser>();
+        [Parameter] public string ContactName { get; set; }
+        [Parameter] public string ContactId { get; set; }
+        private async Task SubmitAsync()
+        {
+            if (!string.IsNullOrEmpty(CurrentMessage) && !string.IsNullOrEmpty(ContactId))
+            {
+                
+                var chatHistory = new ChatMessage()
+                {
+                    Message = CurrentMessage,
+                    ToUserId = ContactId,
+                    CreatedDate = DateTime.Now
+
+                };
+                await _chatManager.SaveMessageAsync(chatHistory);
+                chatHistory.FromUserId = CurrentUserId;
+                await hubConnection.SendAsync("SendMessageAsync", chatHistory, CurrentUserName);
+                CurrentMessage = string.Empty;
+            }
+        }
+        
+    }
+}
