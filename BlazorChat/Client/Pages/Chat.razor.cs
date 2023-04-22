801e949696ae63274d356466ef7f310497eea0db
Kirill Skorikov <92673983+skorikovkr@users.noreply.github.com>
2023-04-22 16:22:29 +0500

    добавил получение сообщений от сервера и получение пользователей, с которыми был диалог

--- BlazorChat/Client/Pages/Chat.razor.cs
+++ BlazorChat/Client/Pages/Chat.razor.cs
@@ -17,8 +17,8 @@ namespace BlazorChat.Client.Pages
         [Parameter] public string CurrentMessage { get; set; }
         [Parameter] public string CurrentUserId { get; set; }
         [Parameter] public string CurrentUserName { get; set; }
-
-       
+        private List<ChatMessage> messages = new List<ChatMessage>();
+        private List<ChatMessage> allMessages = new List<ChatMessage>();
         public List<ApplicationUser> ChatUsers = new List<ApplicationUser>();
         [Parameter] public string ContactName { get; set; }
         [Parameter] public string ContactId { get; set; }
@@ -40,6 +40,95 @@ namespace BlazorChat.Client.Pages
                 CurrentMessage = string.Empty;
             }
         }
-        
+
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await _jsRuntime.InvokeAsync<string>("ScrollToBottom", "chatContainer");
+        }
+        protected override async Task OnInitializedAsync()
+        {
+            if (hubConnection == null)
+            {
+                hubConnection = new HubConnectionBuilder().WithUrl(_navigationManager.ToAbsoluteUri("/signalRHub")).Build();
+            }
+            if (hubConnection.State == HubConnectionState.Disconnected)
+            {
+                await hubConnection.StartAsync();
+            }
+            var state = await _stateProvider.GetAuthenticationStateAsync();
+            var user = state.User;
+            CurrentUserId = user.Claims.Where(a => a.Type == "sub").Select(a => a.Value).FirstOrDefault();
+            CurrentUserName = user.Claims.Where(a => a.Type == "name").Select(a => a.Value).FirstOrDefault();
+
+            hubConnection.On<ChatMessage, string>("ReceiveMessage", async (message, userName) =>
+            {
+                if ((ContactId == message.ToUserId && CurrentUserId == message.FromUserId) || (ContactId == message.FromUserId && CurrentUserId == message.ToUserId))
+                {
+                    if ((ContactId == message.ToUserId && CurrentUserId == message.FromUserId))
+                    {
+                        var newMessage = new ChatMessage
+                        {
+                            Message = message.Message,
+                            CreatedDate = message.CreatedDate,
+                            ToUserId = message.ToUserId,
+                            FromUserId = message.FromUserId,
+                            FromUser = new ApplicationUser() { UserName = CurrentUserName }
+                        };
+                        messages.Add(newMessage);
+                        allMessages.Add(newMessage);
+                        await hubConnection.SendAsync("ChatNotificationAsync", $"Новое сообщение от {userName}:\n{message.Message}...", ContactId, CurrentUserId);
+                    }
+                    else if ((ContactId == message.FromUserId && CurrentUserId == message.ToUserId))
+                    {
+                        var newMessage = new ChatMessage
+                        {
+                            Message = message.Message,
+                            CreatedDate = message.CreatedDate,
+                            ToUserId = message.ToUserId,
+                            FromUserId = message.FromUserId,
+                            FromUser = new ApplicationUser() { UserName = ContactName }
+                        };
+                        messages.Add(newMessage);
+                        allMessages.Add(newMessage);
+                    }
+                    await _jsRuntime.InvokeAsync<string>("ScrollToBottom", "chatContainer");
+                    StateHasChanged();
+                }
+            });
+
+            await GetUsersAsync();
+            foreach (var chatUser in ChatUsers)
+            {
+                var contact = await _chatManager.GetUserDetailsAsync(chatUser.Id);
+                var contactId = contact.Id;
+                var result = await _chatManager.GetConversationAsync(contactId);
+                foreach (var message in result)
+                {
+                    allMessages.Add(message);
+                }
+            }
+            if (!string.IsNullOrEmpty(ContactId))
+            {
+                await LoadUserChat(ContactId);
+            }
+            StateHasChanged();
+        }
+
+        async Task LoadUserChat(string userId)
+        {
+            var contact = await _chatManager.GetUserDetailsAsync(userId);
+            ContactId = contact.Id;
+            ContactName = contact.UserName;
+            _navigationManager.NavigateTo($"chat/{ContactId}");
+
+            messages = await _chatManager.GetConversationAsync(ContactId);
+        }
+
+
+        private async Task GetUsersAsync()
+        {
+            ChatUsers = await _chatManager.GetUsersAsync();
+        }
     }
 }
